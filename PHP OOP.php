74715e202d87f1544acbadf04ae3e16163d2824e
justin munoz <jayymunoz66@gmail.com>
2015-01-07 12:31:53 -0800

    examples

---
+++ PHP OOP.php
@@ -0,0 +1,84 @@
+<?php
+
+//example//
+  class king{
+      //body
+      //body
+      //body
+  }
+  
+  class mvp{
+      //body
+      //body
+      //body
+  }
+  
+  class soccer{
+      //body
+      //body
+      //body
+  }
+  //example2//
+  $king1 = new king();
+  $king2 = new king();
+  
+  $mvp1 = new mvp();
+  $mvp2= new mvp();
+  
+  
+  $soccer1 = new soccer();
+  $soccer2 = new soccer();
+  //example3//
+  
+  class king1{
+      public $name = "andre";
+      public $gender = "male";
+      public $cleats = "adidas";
+              
+  }
+  
+  class mvp1{
+      public $fifa = "best";
+      public $xbox = "beast";
+      public $gym = "buff";
+      
+  }
+  
+  class soccer1{
+      public $futbol = "nike";
+      public $gol = "golazo";
+      public $speed = "fast";
+  
+  
+//example3//
+      
+      class car {
+          
+          public $chevy = "chevy";
+          
+          public $ferarri ="ferrari";
+          
+          public $model = "model";
+          
+          public $2,300 = 2,300;
+          
+          function getName() {
+              
+             return "{$this>chevy} " . 
+                     
+                     return "{$this>ferrari}";
+        
+      }
+
+      
+      }
+      
+      $car1 = new car();
+      
+      $car1 >ferrari = "ferrari";
+      
+      $dog1>silverado = "silverado";
+      
+      print "the cars name is {$car>ferrarimilan()}.""
+          //the cars name is chevy silverado
+      
