d933753d97cd82f4add13f9b6a6651f15e4ca5c7
justin munoz <jayymunoz66@gmail.com>
2015-01-08 12:22:02 -0800

    lectures

--- PHP OOP.php
+++ PHP OOP.php
@@ -81,3 +81,42 @@
       
       print "the cars name is {$car>ferrarimilan()}.""
           //the cars name is ferrarimilan
+          
+
+
+
+  <-------------lecture2----------->
+  <-------------example1----------->
+  
++
++class Dog {
++public $rex;
++public $tom;
++public $german sheperd;
++function __construct($title, $rex, $tom, $german sheperd) {
++$this>firstName = $rex;
++$this>lastName = $tom;
++$this>breed = $german sheperd;
++}
++function getName() {
++return "{$this>rex}" . 
++"{$this>tom}"; . 
++                     }
++}
+
+<------example2------>
+
++class laptop {
++public $macbook;
++public $pro;
++public $apple;
++function__construct($title, $macbook, $pro, $apple) {
++$this>mackbook = $macbook;
++$this>apple = $pro;
++}
++function getName() {
++return "{$this>macbook}" . 
++"{$this>pro}";
++                 }
++}
++
