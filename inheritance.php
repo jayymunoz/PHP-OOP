2ef10b9541772d2a2b9d44c03fe8b127aef5dbeb
justin munoz <jayymunoz66@gmail.com>
2015-01-14 12:30:37 -0800

    LEC 5

--- inheritance.php
+++ inheritance.php
@@ -34,11 +34,60 @@ class Futbol {
             
         }
       }   
-           class club extends fifa {
+           class club extends fifa 
+      
+               
+           }
                function greet () {
                    return $this->coach;
                
            
         }
         
+        class Nike 
+        
+        
+        public $shoes;
+        public $shirts;
+        public $pants;
+        public $sweats;
+        public $hat;
+        public $socks;
+        public $ball;
+}
+             function __construct($shoes, $shirts, $pants, $sweats, $hat, $socks, $ball ) {
+              $this->jersey = $shoes;
+              $this->ronaldo = $shirts;
+              $this->cleats = $pants;
+              $this->schurrle = $sweats;
+              $this->chelsea = $hat;
+              $this->england = $socks;
+              $this->ball = $ball;
+             }
+             
+             function getName() {
+             return "this is my" . $this-shoes . 
+             " and last " . $this-> shirts;
+             
+             }
+             
+          
+        
+        
+        
+        class soccer extends sports {
+        function greet () {
+            return $this->player;
+            
+        }
+      }   
+           class club extends fifa 
+      
+               
+           }
+               function greet () {
+                   return $this->coach;
+               
+           
+        }
      ?>
